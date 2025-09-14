308f70659b21617df22cb6e93c7c0f1a0c413f6f
samazolfkhani-dev <samazolfkhani.work@gmail.com>
2025-09-14 04:01:46 -0700

    Lv1-Types Of print command

---
+++ learning_python/extra/type_of_print_commands/Ex1.py
@@ -0,0 +1,26 @@
+#simple print
+print("Sama Zolfkhani")
+
+#print some variables by comma
+print("Name: ","Aseman","  Age: ",19)
+
+#change seperator by sep
+print("2025","12","10",sep="-")
+
+#preventing new line by end
+print("Hello",end=" ")
+print("Sama")
+
+#print by f-string
+x=10
+y=2
+print(f"x+y={x+y}")
+
+#print by .format()
+name="sky"
+age=19
+print("Hello Iam {} and Iam {}".format(name,age))
+
+name="sky"
+age=19
+print("My name is %s And Iam %d years old" %(name,age))
