3d1328a35c68bf57c232bdfb5724c8179bc60118
samazolfkhani-dev <samazolfkhani.work@gmail.com>
2025-09-14 04:00:39 -0700

    Lv1-Types Of print command

---
+++ learning_python/data_type/primitive_data_type/Ex1.py
@@ -0,0 +1,12 @@
+word="Python"
+print(word.isalpha())
+word="Python3"
+print(word.isalpha())
+
+number="1234"
+print(number.isnumeric())
+number="1a23"
+print(number.isnumeric())
+
+x="123sama"
+print(x.isalnum())
